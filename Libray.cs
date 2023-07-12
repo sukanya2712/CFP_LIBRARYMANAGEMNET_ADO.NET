e754568dede5c1fef44ea82d9aaf68921707e8ae
sukanya2712 <sukanyanaik511@gmail.com>
2023-07-13 00:21:01 +0530

    started library managemnt

---
+++ Libray.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementAdo
+{
+    public class Libray
+    {
+        private string connection = $"Data source= DESKTOP-41GBJMF; Database = LibraryManagement; Integrated Security = true; TrustServerCertificate = true";
+        private SqlConnection sqlConnection;
+        public Libray()
+        {
+            sqlConnection = new SqlConnection(connection);
+        }
+    }
+}
